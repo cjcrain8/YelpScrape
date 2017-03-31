2a9a7b38b99aa7789432fc30f5e966e6abb59071
Chelsea Crain <chelsea.crain@gmail.com>
2017-03-31 13:10:16 -0500

    Update run_scrape_restart.cmd

--- run_scrape_restart.cmd
+++ run_scrape_restart.cmd
@@ -6,7 +6,7 @@ REM "C:\Users\cjcrain\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Syst
 REM D:\Yelp\Scrape8_0217\reset_hotspot.bat
 
 TIMEOUT 160
-"C:\Program Files\Anaconda2\Scripts\ipython.exe" D:\Yelp\Yelp_Scrape\yelp_scrape9.py %*
+"C:\Program Files\Anaconda2\Scripts\ipython.exe" D:\Yelp\Yelp_Scrape\yelp_scrape9.py %1
 
 
 REM "C:\Users\Chelsea\Anaconda2\Scripts\ipython.exe" E:\Yelp\Yelp_Scrapes\Scrape9_0317\scrape9.py %*
